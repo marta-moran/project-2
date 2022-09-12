9fcdf0476015a79f27d0368005d900c46ac73108
Marta Morán <martamoranl06@gmail.com>
2022-09-12 17:02:32 +0200

    CRUD users

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,7 +1,53 @@
 const router = require("express").Router();
+const User = require('../models/User.model')
 
-router.get('/users', (req, res, next) => {
-    res.render('list-users')
+router.get('/', (req, res, next) => {
+    User.find()
+    User.find()
+        .then(users => {
+            res.render('users/list-users', { users })
+        })
+        .catch(error => next(error))
+})
+
+router.get('/:id', (req, res, next) => {
+    User.findById(req.params.id)
+        .then((user) => {
+            res.render('users/view-user', user)
+        })
+        .catch((err) => {
+            next(err);
+        });
+})
+
+router.get('/:id/edit', (req, res, next) => {
+    User.findById(req.params.id)
+        .then((user) => {
+            res.render('users/edit-form', user)
+        })
+})
+
+router.post('/:id/edit', (req, res, next) => {
+    const { username } = req.body
+    User.findByIdAndUpdate(req.params.id, { username })
+        .then((userUpdate) => {
+            console.log(userUpdate)
+            res.redirect('/users')
+        })
+        .catch((err) => {
+            next(err);
+        });
+})
+
+router.get('/:id/delete', (req, res, next) => {
+    User.findByIdAndDelete(req.params.id)
+        .then((deleteUser) => {
+            console.log(deleteUser)
+            res.redirect('/users')
+        })
+        .catch((err) => {
+            next(err);
+        });
 })
 
 module.exports = router;
