d50904040f0fc61933bbaeeaa9ecb03e0c747f11
Jaime Martin <martinjaime1119@gmail.com>
2022-09-13 12:43:18 +0200

    navbar edit image

--- middleware/setLocals.middleware.js
+++ middleware/setLocals.middleware.js
@@ -1,8 +1,8 @@
 const setLocals = (req, res, next) => {
     req.app.locals.isLogged = req.session.currentUser ? true : false
     if (req.app.locals.isLogged) {
-        req.app.locals.avatar = req.session.currentUser.avatar
-        req.app.locals.username = req.session.currentUser.username
+        req.app.locals.currentAvatar = req.session.currentUser.avatar
+        req.app.locals.currentUsername = req.session.currentUser.username
         req.app.locals.id = req.session.currentUser._id
     } next()
 }
